03188b926fe6f4bac56fcd18b25c2675309bd9f5
Cristian Crasneanu <crasneanu.cristian@gmail.com>
2023-03-14 13:48:30 +0200

    [ Project ] - Add initial files

---
+++ Jokes/Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Cristian Crasneanu on 14.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
